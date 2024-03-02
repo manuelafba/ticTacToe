870d922bc8abf801c4e79c5076a1d2760c0c92b3
Manuela Araújo <mxnuelafba@gmail.com>
2024-03-02 15:07:00 -0300

    funções para validar caracteres e coordenadas

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 using namespace std;
 
-void inicializarTabuleiro(char tabuleiro[3][3]){
+char tabuleiro[3][3];
+
+void inicializarTabuleiro(){
 
     int linha, coluna;
 
@@ -10,7 +12,32 @@ void inicializarTabuleiro(char tabuleiro[3][3]){
         for (coluna = 0; coluna < 3; coluna++){
             tabuleiro[linha][coluna] = '-';
         }
+    }
+}
 
+int validarCaractere(char letra) {
+    if (letra == 'X' || letra == 'O') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int validarCoordenada(int x, int y) {
+    if (x >= 0 && x < 3) {
+        if (y >= 0 && y < 3) {
+            return 1;
+        }
+    } else {
+        return 0;
+    }
+}
+
+int verificarPosicaoVazia(int x, int y) {
+    if (tabuleiro[x][y] != 'X' && tabuleiro[x][y] != '0') {
+        return 1;
+    } else {
+        return 0;
     }
 }
 
