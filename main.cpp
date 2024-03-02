acb2df018475ec73a6474534ad06dcecaf765b24
Manuela Araújo <mxnuelafba@gmail.com>
2024-03-02 14:41:35 -0300

    função inicializar tabuleiro

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 using namespace std;
 
-void iniciarTabuleiro(char tabuleiro[3][3]){
+void inicializarTabuleiro(char tabuleiro[3][3]){
 
     int linha, coluna;
 
@@ -14,19 +14,6 @@ void iniciarTabuleiro(char tabuleiro[3][3]){
     }
 }
 
-void imprimirTabuleiro(char tabuleiro[3][3]) {
-    int linha, coluna;
-
-    for (linha = 0; linha < 3; linha++){
-        for (coluna = 0; coluna < 3; coluna++) {
-            cout << tabuleiro[linha][coluna];
-        }
-        cout << endl;
-    }
-}
-
 int main() {
-    char tabuleiro[3][3], jogador;
-    int i, j, linha, coluna, rodada, ganhou;
 
 }
